57045ba215829df9d7cc6caa98a87065a8ab8e10
Daniel Finnegan <D.J.Finnegan@bath.ac.uk>
2019-10-30 16:20:39 +0000

    Hoping the problem is resolved when the working directory is taken from the scope of the function call to dual_column

--- R/dual_column.R
+++ R/dual_column.R
@@ -1,11 +1,12 @@
 ## A handy function for creating dual column slides in both html and powerpoint
-dual_column <- function (left.is.text=TRUE, left, right, img) {
+dual_column <- function (left.is.text=TRUE, left, right, img, working_dir=getwd()) {
   if (knitr::is_html_output()) {
     cat(knitr::knit_child(system.file('extrmdfiles', 'dual-column-html.Rmd', package = 'columnizeR'),
                           options=list (content.type=left.is.text,
                                         left_content_path=left,
                                         right_content_path=right,
-                                        img_content_path=img),
+                                        img_content_path=img,
+                                        wd=working_dir),
                           quiet=TRUE),
         sep = "\n")
   } else {
@@ -13,7 +14,8 @@ dual_column <- function (left.is.text=TRUE, left, right, img) {
                           options=list (content.type=left.is.text,
                                         left_content_path=left,
                                         right_content_path=right,
-                                        img_content_path=img),
+                                        img_content_path=img,
+                                        wd=working_dir),
                           quiet=TRUE))
   }
 }
