92e9ac6abfe3c516394bdcea62ac90a5b8cda147
Daniel Finnegan <D.J.Finnegan@bath.ac.uk>
2019-10-30 17:06:37 +0000

    Added documentation for the package

--- R/dual_column.R
+++ R/dual_column.R
@@ -1,6 +1,17 @@
-## A handy function for creating dual column slides in both html and powerpoint
-dual_column <- function (left.is.text=TRUE, left, right, img) {
-  working_dir <- getwd()
+#' Render two input Rmd files as equal width columns.
+#'
+#' Note this function is rather limited: it is expected for use inside R chunks with the results='asis' and warning=FALSE options specified.
+#'
+#' @param left.is.text True if the content of the left file is text only.
+#' @param left The path to the local Rmd file for the left column.
+#' @param right The path to the local Rmd file for the right column.
+#' @param img path to the local image file to insert into the right column.
+#' @param working_dir The working directory to base relative paths from
+#' @return Dual column markdown correctly rendered using knitr irrespective of the output format.
+#' @examples
+#' dual_column (left='left-file.Rmd', right='right-file.Rmd', img='image-file.jpg', working_dir='./')
+#' dual_column (left.is.text=FALSE, left='left-file.Rmd', right='right-file.Rmd', img='image-file.jpg')
+dual_column <- function (left.is.text=TRUE, left, right, img, working_dir=getwd()) {
   if (knitr::is_html_output()) {
     cat(knitr::knit_child(system.file('extrmdfiles', 'dual-column-html.Rmd', package = 'columnizeR'),
                           options=list (content.type=left.is.text,
---
+++ README.md
@@ -0,0 +1,10 @@
+# columnizeR
+
+A small package for rendering equal width dual columns.
+
+## Rationale
+
+I ended up doing a lot of dual column layouts for seminars, teaching etc that I wanted to knit into powerpoint, HTML, and beamer presentations.
+I got frustrated doing it manually each time.
+So I wrote this.
+Hopefully you (whoever you are) find it useful too.
