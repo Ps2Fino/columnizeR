4ce4193eb97f768a807a468c0e73c92a938d2387
Daniel Finnegan <D.J.Finnegan@bath.ac.uk>
2019-10-30 16:35:15 +0000

    Trying a call to getwd before the knit functions are called

--- R/dual_column.R
+++ R/dual_column.R
@@ -1,5 +1,6 @@
 ## A handy function for creating dual column slides in both html and powerpoint
-dual_column <- function (left.is.text=TRUE, left, right, img, working_dir=getwd()) {
+dual_column <- function (left.is.text=TRUE, left, right, img) {
+  working_dir <- getwd()
   if (knitr::is_html_output()) {
     cat(knitr::knit_child(system.file('extrmdfiles', 'dual-column-html.Rmd', package = 'columnizeR'),
                           options=list (content.type=left.is.text,
