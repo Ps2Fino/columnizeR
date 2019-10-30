6efa2719b30ac229e7aebfaa2538be376afe6a8e
Daniel Finnegan <D.J.Finnegan@bath.ac.uk>
2019-10-30 13:56:19 +0000

    Initial commit

---
+++ R/dual_column.R
@@ -0,0 +1,19 @@
+## A handy function for creating dual column slides in both html and powerpoint
+dual_column <- function (left.is.text=TRUE, left, right, img) {
+  if (knitr::is_html_output()) {
+    cat(knitr::knit_child(system.file('extrmdfiles', 'dual-column-html.Rmd', package = 'columnizeR'),
+                          options=list (content.type=left.is.text,
+                                        left_content_path=left,
+                                        right_content_path=right,
+                                        img_content_path=img),
+                          quiet=TRUE),
+        sep = "\n")
+  } else {
+    cat(knitr::knit_child(system.file('extrmdfiles', 'dual-column-pp-latex.Rmd', package = 'columnizeR'),
+                          options=list (content.type=left.is.text,
+                                        left_content_path=left,
+                                        right_content_path=right,
+                                        img_content_path=img),
+                          quiet=TRUE))
+  }
+}
